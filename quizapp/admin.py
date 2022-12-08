44498f90b06685064c6342d138406369ec397048
LaLluviadelJulio <inspiracion@yandex.ru>
2022-12-08 18:46:12 +0300

    - added Question model; - added QuestionSet model; - added the mechanism for editing a set of questions in the admin panel; - added custom admin panel templates;

--- quizapp/admin.py
+++ quizapp/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
+from django.contrib.admin.options import InlineModelAdmin
+from django.contrib.contenttypes.admin import GenericTabularInline, GenericStackedInline
+from django.forms import ModelForm, Textarea
 
-from quizapp.models import Category
+from quizapp.models import Category, Question, QuestionSet
+
 
 admin.site.site_header = 'Админ-панель тестового задания для ИП Авдеев В.Ю. "'
+admin.site.site_title = 'Тестовое задание для ИП Авдеев В.Ю. "'
+
+class BigTextBodyForm(ModelForm):
+    """A form for the question set admin panel that uses a more convenient widget for large text."""
+
+    class Meta:
+        """Defining a widget for a field."""
+        model = Question
+        widgets = {
+            'text': Textarea
+        }
+        fields = '__all__'
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -12,4 +28,31 @@ class CategoryAdmin(admin.ModelAdmin):
     list_filter = ('is_active',)
     fields = (('title', 'is_active'), 'description', )
 
+    def delete_queryset(self, request, queryset):
+        """Defines the behavior when deleting a set.
+        The object will not be deleted, but deactivated."""
+        for item in queryset:
+            item.delete()
+
+
+class QuestionInline(GenericStackedInline):
+    """A class for working with the Question model in the admin panel (inline,
+    as part of editing QuestionSet)."""
+    model = Question
+    form = BigTextBodyForm
+    fields = ('text', ('category', 'is_active'),
+              ('answer_01', 'answer_02', 'answer_03', 'answer_04', ),
+              'right_answers',)
+    extra = 1
+
+
+class QuestionSetAdmin(admin.ModelAdmin):
+    """A class for working with the QuestionSet model in the admin panel."""
+    list_display = ('title', 'is_active')
+    search_fields = ('title',)
+    list_filter = ('is_active',)
+    fields = (('title', 'is_active'),)
+    inlines = [QuestionInline, ]
+
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(QuestionSet, QuestionSetAdmin)
--- quizapp/models.py
+++ quizapp/models.py
@@ -1,6 +1,8 @@
 import logging
 from logging import Logger
 
+from django.contrib.contenttypes.fields import GenericRelation, GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import IntegrityError
 from django.urls import reverse
 from django.utils import timezone
@@ -11,8 +13,8 @@ logger: Logger = logging.getLogger(__name__)
 
 
 class BaseModel(models.Model):
-    """Base class for Category and ... models."""
-    title = models.CharField(max_length=150, unique=True, verbose_name='наименование')
+    """Base class for Category and QuestionSet models."""
+    title = models.CharField(max_length=250, unique=True, verbose_name='наименование')
     create_time = models.DateTimeField(default=timezone.now, verbose_name="время создания")
     update_time = models.DateTimeField(default=timezone.now, verbose_name="время изменения")
     is_active = models.BooleanField(default=True, db_index=True, verbose_name="активен")
@@ -48,13 +50,14 @@ class BaseModel(models.Model):
         return reverse(urlpattern_name, args=[str(self.slug)])
 
     def delete(self, using=None, keep_parents=False):
-        self.is_active = False if self.is_active is False else True
+        """The object will not be deleted, but deactivated."""
+        self.is_active = not self.is_active
         self.save()
 
 
 class Category(BaseModel):
     """The model for the category."""
-    description = models.TextField(blank=True)
+    description = models.TextField(blank=True, verbose_name="описание")
 
     class Meta:
         """Ordering categories according to their id."""
@@ -67,8 +70,55 @@ class Category(BaseModel):
         """
         super().save(*args, slugified_field=self.title, **kwargs)
 
-
     def get_absolute_url(self, urlpattern_name='category_read'):
         """Returns formed url for the object."""
         return super().get_absolute_url(urlpattern_name=urlpattern_name)
 
+
+class Question(models.Model):
+    """The model for the question."""
+
+    text = models.CharField(max_length=250, default='question', verbose_name="текст вопроса")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default='1', verbose_name="категория")
+    create_time = models.DateTimeField(default=timezone.now, verbose_name="время создания")
+    update_time = models.DateTimeField(default=timezone.now, verbose_name="время изменения")
+    is_active = models.BooleanField(default=True, db_index=True, verbose_name="активен")
+
+    right_answers = models.CharField(max_length=50, default='1,', verbose_name="правильный ответ/ответы")
+    answer_01 = models.CharField(max_length=150, default='default', verbose_name="ответ №1")
+    answer_02 = models.CharField(max_length=150, default='default', verbose_name="ответ №2")
+    answer_03 = models.CharField(max_length=150, default='default', verbose_name="ответ №3")
+    answer_04 = models.CharField(max_length=150, default='default', verbose_name="ответ №4")
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE,)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    class Meta:
+        """Ordering questions according to their id."""
+        ordering = ('id',)
+        verbose_name = 'Вопрос'
+        verbose_name_plural = 'Вопросы'
+
+    def __str__(self):
+        """Forms and returns a printable representation of the object."""
+        return str(self.text)
+
+class QuestionSet(BaseModel):
+    """The model for the question set."""
+    questions = GenericRelation('Question')
+
+    class Meta:
+        """Ordering questions according to their id."""
+        ordering = ('id',)
+        verbose_name = 'Набор тестов'
+        verbose_name_plural = 'Наборы тестов'
+
+    def save(self, *args, **kwargs):
+        """Automatic filling in update_time and the slug field when saving.
+        """
+        super().save(*args, slugified_field=self.title, **kwargs)
+
+    def get_absolute_url(self, urlpattern_name='questionset_read'):
+        """Returns formed url for the object."""
+        return super().get_absolute_url(urlpattern_name=urlpattern_name)
